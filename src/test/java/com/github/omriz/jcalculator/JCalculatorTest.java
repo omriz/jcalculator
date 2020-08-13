5b3698a7fa9b432b9bdade407c41c285da71623e
Omri Zohar <omri.zohar@gmail.com>
2020-08-13 12:07:30 +0300

    Adding E2E test

--- src/test/java/com/github/omriz/jcalculator/JCalculatorTest.java
+++ src/test/java/com/github/omriz/jcalculator/JCalculatorTest.java
@@ -3,6 +3,10 @@ package com.github.omriz.jcalculator;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -15,36 +19,23 @@ public class JCalculatorTest {
     public void setupJCalculator() {
         jCalculator = new JCalculator();
     }
-
     @Test
-    public void testSimpleStatement() {
-        assertEquals(Integer.valueOf(5), jCalculator.ProcessLineValue("5"));
-    }
-
-
-    @Test
-    public void testCalculateParenthesis() {
-        jCalculator.ProcessLine("i=(1)");
-        jCalculator.ProcessLine("j=((++i))");
-        Map<Character, Integer> expected = Map.of('i', 2, 'j', 2);
-        assertTrue(expected.equals(jCalculator.getVariables()));
-    }
-
-    @Test
-    public void testSimpleDivision() {
-        jCalculator.ProcessLine("i=8");
-        jCalculator.ProcessLine("j=4");
-        jCalculator.ProcessLine("k = i / j");
-        Map<Character, Integer> expected = Map.of('i', 8, 'j', 4, 'k', 2);
-        assertTrue(expected.equals(jCalculator.getVariables()));
-    }
-
-    @Test
-    public void testSimpleAddition() {
-        jCalculator.ProcessLine("i=8");
-        jCalculator.ProcessLine("j=4");
-        jCalculator.ProcessLine("k = i + j");
-        Map<Character, Integer> expected = Map.of('i', 8, 'j', 4, 'k', 12);
+    public void testE2E() {
+        String resourceName = "calculation.txt";
+        ClassLoader classLoader = getClass().getClassLoader();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(classLoader.getResource(resourceName).getFile()));
+            String line = reader.readLine();
+            while (line != null) {
+                jCalculator.ProcessLine(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Map<Character, Integer> expected = Map.of('i', 8, 'j',4,'k',9, 'f',1);
         assertTrue(expected.equals(jCalculator.getVariables()));
     }
 }
